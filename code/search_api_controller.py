54c9dfeb03f1247b1e3d0f871f1ac686372c5a8b
Sayali Parab <sayaliparab0494@gmail.com>
2021-09-18 00:17:28 -0400

    Adding basic web search for Google Web API

---
+++ code/search_api_controller.py
@@ -0,0 +1,10 @@
+from google_search import *
+
+# Response of the Google web API Search
+google_search_results = search_call("hello")
+
+# Response of the Rapid web API Search
+# rapidapi_search_results =
+
+# Response of the API Search
+print(google_search_results)
