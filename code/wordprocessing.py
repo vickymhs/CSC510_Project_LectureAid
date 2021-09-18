bd3194abcbe82aeab25b5bd3c8271266f7b75b8f
Manoj Kumar <mtkumar@ncsu.edu>
2021-09-18 14:03:21 -0400

    Change wordprocessing.py

--- code/wordprocessing.py
+++ code/wordprocessing.py
@@ -79,45 +79,23 @@ def merge_slide_with_same_headers(data: list) -> list:
     return merged
 
 
-def merge_slide_with_same_slide_number(data: list) -> list:
-    '''
-
-    :param data:
-    :return:
-    '''
-    merged = []
-    slide_number = []
-    for slide in data:
-        if slide["slide"] not in slide_number:
-            slide_number.append(slide["slide"])
-            header_keywords = []
-            paragraph_keywords = []
-            for x in [y for y in data if y["slide"] == slide["slide"]]:
-                header_keywords += x["Header_keywords"]
-                paragraph_keywords += x["Paragraph_keywords"]
-            merged.append({"Header": slide["Header"], "Header_keywords": header_keywords,
-                           "Paragraph_keywords": paragraph_keywords,
-                           "slide": slide["slide"]})
-    return merged
-
-
 if __name__ == "__main__":
     main_data = [{"header": "Dimensionality Reduction PCA",
-                  "paragraph": "Dimensionality Reduction Purposes: – Avoid curse of dimensionality – Reduce amount \
+             "paragraph": "Dimensionality Reduction Purposes: – Avoid curse of dimensionality – Reduce amount \
             of time and memory required by data mining algorithms Allow data to be more easily \
             visualized May help to eliminate irrelevant features or reduce noise Techniques Principal Component Analysis \
             Singular Value Decomposition supervised and non-linear techniques",
-                  "slide": 8},
-                 {"header": "Gratuitous ARP",
-                  "paragraph": "Every machine broadcasts its mapping when it boots to update ARP caches in other machines \
+             "slide": 8},
+            {"header": "Gratuitous ARP",
+             "paragraph": "Every machine broadcasts its mapping when it boots to update ARP caches in other machines \
             n Example: A sends an ARP Request with its own IP address as the target IP address \
             n Sender MAC=MACA, Sender IP=IPA n Target MAC=??, Target IP=IPA \
             n What if a reply is received?",
-                  "slide": 9},
-                 {"header": "Dimensionality Reduction PCA",
-                  "paragraph": "Goal is to find a projection that captures the largest amount of variation in data \
+             "slide": 9},
+            {"header": "Dimensionality Reduction PCA",
+             "paragraph": "Goal is to find a projection that captures the largest amount of variation in data \
              Find the eigenvectors of the covariance matrix The eigenvectors define the new space",
-                  "slide": 10}]
+             "slide": 10}]
 
     keyword_data = keyword_extractor(main_data)
     print(keyword_data)
