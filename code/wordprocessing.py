9ee64b78f7be346d327f3ca490624df16b5d5f53
Manoj Kumar <mtkumar@ncsu.edu>
2021-09-18 14:06:38 -0400

    change wordprocessing.py

--- code/wordprocessing.py
+++ code/wordprocessing.py
@@ -19,8 +19,8 @@ def keyword_extractor(data: list) -> list:
         sys.exit()
     pos_tag = ["PROPN", "NOUN"]
     for slide in data:
-        doc_header = nlp(slide["header"].lower())
-        doc_paragraph = nlp(slide["paragraph"].lower())
+        doc_header = nlp(slide["Header"].lower())
+        doc_paragraph = nlp(slide["Paragraph"].lower())
         header_keywords = []
         paragraph_keywords = []
         for token in doc_header:
@@ -37,8 +37,8 @@ def keyword_extractor(data: list) -> list:
                 word = re.sub(r"[^0-9a-zA-Z]+", "", token.text)
                 if word != "":
                     paragraph_keywords.append(word)
-        slide["header_keywords"] = header_keywords
-        slide["paragraph_keywords"] = paragraph_keywords
+        slide["Header_keywords"] = header_keywords
+        slide["Paragraph_keywords"] = paragraph_keywords
     return data
 
 
@@ -51,8 +51,8 @@ def duplicate_word_removal(data: list) -> list:
                 "paragraph_keywords": [], slides:[int]}]
     """
     for dictionary in data:
-        dictionary['header_keywords'] = list(OrderedDict.fromkeys(dictionary['header_keywords']))
-        dictionary['paragraph_keywords'] = list(OrderedDict.fromkeys(dictionary['paragraph_keywords']))
+        dictionary['Header_keywords'] = list(OrderedDict.fromkeys(dictionary['Header_keywords']))
+        dictionary['Paragraph_keywords'] = list(OrderedDict.fromkeys(dictionary['Paragraph_keywords']))
     return data
 
 
@@ -67,35 +67,57 @@ def merge_slide_with_same_headers(data: list) -> list:
     merged = []
     headers = []
     for slide in data:
-        if slide["header"] not in headers:
-            headers.append(slide["header"])
+        if slide["Header"] not in headers:
+            headers.append(slide["Header"])
             paragraph_keywords = []
             slide_numbers = []
-            for x in [y for y in data if y["header"] == slide["header"]]:
-                paragraph_keywords += x["paragraph_keywords"]
+            for x in [y for y in data if y["Header"] == slide["Header"]]:
+                paragraph_keywords += x["Paragraph_keywords"]
                 slide_numbers.append(x["slide"])
-            merged.append({"header": slide["header"], "header_keywords": slide["header_keywords"],
-                           "paragraph_keywords": paragraph_keywords, "slides": slide_numbers})
+            merged.append({"Header": slide["Header"], "Header_keywords": slide["Header_keywords"],
+                           "Paragraph_keywords": paragraph_keywords, "slides": slide_numbers})
+    return merged
+
+
+def merge_slide_with_same_slide_number(data: list) -> list:
+    '''
+
+    :param data:
+    :return:
+    '''
+    merged = []
+    slide_number = []
+    for slide in data:
+        if slide["slide"] not in slide_number:
+            slide_number.append(slide["slide"])
+            header_keywords = []
+            paragraph_keywords = []
+            for x in [y for y in data if y["slide"] == slide["slide"]]:
+                header_keywords += x["Header_keywords"]
+                paragraph_keywords += x["Paragraph_keywords"]
+            merged.append({"Header": slide["Header"], "Header_keywords": header_keywords,
+                           "Paragraph_keywords": paragraph_keywords,
+                           "slide": slide["slide"]})
     return merged
 
 
 if __name__ == "__main__":
-    main_data = [{"header": "Dimensionality Reduction PCA",
-             "paragraph": "Dimensionality Reduction Purposes: – Avoid curse of dimensionality – Reduce amount \
+    main_data = [{"Header": "Dimensionality Reduction PCA",
+                  "Paragraph": "Dimensionality Reduction Purposes: – Avoid curse of dimensionality – Reduce amount \
             of time and memory required by data mining algorithms Allow data to be more easily \
             visualized May help to eliminate irrelevant features or reduce noise Techniques Principal Component Analysis \
             Singular Value Decomposition supervised and non-linear techniques",
-             "slide": 8},
-            {"header": "Gratuitous ARP",
-             "paragraph": "Every machine broadcasts its mapping when it boots to update ARP caches in other machines \
+                  "slide": 8},
+                 {"Header": "Gratuitous ARP",
+                  "Paragraph": "Every machine broadcasts its mapping when it boots to update ARP caches in other machines \
             n Example: A sends an ARP Request with its own IP address as the target IP address \
             n Sender MAC=MACA, Sender IP=IPA n Target MAC=??, Target IP=IPA \
             n What if a reply is received?",
-             "slide": 9},
-            {"header": "Dimensionality Reduction PCA",
-             "paragraph": "Goal is to find a projection that captures the largest amount of variation in data \
+                  "slide": 9},
+                 {"Header": "Dimensionality Reduction PCA",
+                  "Paragraph": "Goal is to find a projection that captures the largest amount of variation in data \
              Find the eigenvectors of the covariance matrix The eigenvectors define the new space",
-             "slide": 10}]
+                  "slide": 9}]
 
     keyword_data = keyword_extractor(main_data)
     print(keyword_data)
