05ef1da39d74adeaa221b5ed487f7cb8862a9568
Renji Joseph Sabu <renjiniravath@gmail.com>
2021-09-26 03:24:04 -0400

    people also ask unit test added

--- test/test_google_search.py
+++ test/test_google_search.py
@@ -1,12 +1,23 @@
 import unittest
 from code.google_search import get_people_also_ask_links
+import json
 
 class TestGoogleSearch(unittest.TestCase):
     def setUp(self) -> None:
         pass
 
-    def test_get_people_also_ask_links(self):
-        """Test the get_people_also_ask_links method"""
+    def test_get_people_also_ask_links_type_check(self):
+        """Test the get_people_also_ask_links method and checks type"""
         test = "principal components"
         result = get_people_also_ask_links(test)
         self.assertEqual(list, type(result))
+
+    def test_get_people_also_ask_links_data_check(self):
+        """Test the get_people_also_ask_links method and checks data"""
+
+        filename = "./test/data/Test_0.json"
+        with open(filename, encoding='utf-8') as file_pointer:
+            doc = json.load(file_pointer)
+            test = "Continuous integration"
+            result = get_people_also_ask_links(test)
+            self.assertEqual(result, doc)
