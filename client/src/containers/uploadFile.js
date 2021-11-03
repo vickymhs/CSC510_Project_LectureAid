f86caedd12d39d6c262002aa792944daca125a48
Rahul Kalita <rahulkalita8@gmail.com>
2021-11-02 22:09:32 -0400

    Adding upload file DOM

---
+++ client/src/containers/uploadFile.js
@@ -0,0 +1,8 @@
+import * as React from 'react';
+import Button from '@mui/material/Button'
+
+export default function UploadFile(props) {
+    return(<div>
+        <Button>Click Me</Button>
+    </div>)
+}
